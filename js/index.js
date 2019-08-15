02e30d528d499cf4ecff40c8218b17fa424dc28f
Santiago Guerra <guerrasantiago19@gmail.com>
2019-08-15 11:20:20 -0500

    js and stickler problems fixed

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//universal selector to reset styles by default
+var allElements = document.getElementsByTagName("*");
+for (var i = 0, len = allElements.length; i < len; i++) {
+    var element = allElements[i];
+    
+    element.style.boxSizing = 'border-box';
+    element.style.fontFamily = 'Roboto'
+}
+
