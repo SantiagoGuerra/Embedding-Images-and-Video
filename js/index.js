983f8cef788f097068f8427f5d3b0ce0696db1e9
Santiago Guerra <guerrasantiago19@gmail.com>
2019-08-15 12:13:43 -0500

    ESLint config rules added

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 //universal selector to reset styles by default
 var allElements = document.getElementsByTagName("*");
+
 for (var i = 0, len = allElements.length; i < len; i++) {
     var element = allElements[i];
     
     element.style.boxSizing = 'border-box';
-    element.style.fontFamily = 'Roboto'
+    element.style.fontFamily = 'Roboto';
 }
 
